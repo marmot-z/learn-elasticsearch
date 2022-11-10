8091c5beef9950e77c8799bcb329cd67070d44a7
zhangxunwei <zhangxunwei@dxy.cn>
2022-11-10 13:18:37 +0800

    feat: 新增 ids query 章节

---
+++ query/term-queries/ids/tests.js
@@ -0,0 +1,9 @@
+const body = pm.response.json();
+
+pm.test('ids query successful', () => {
+    let docIds = body.hits.hits.map(hit => hit._source.id);
+
+    pm.expect(docIds.indexOf(5) > -1).to.be.true;
+    pm.expect(docIds.indexOf(8) == -1).to.be.true;
+    pm.expect(docIds.indexOf(11) > -1).to.be.true;
+});
